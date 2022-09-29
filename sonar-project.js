98b6584f6f2c1d52db901548e7852b449c6cda5a
mekuschi <111418411+mekuschi@users.noreply.github.com>
2022-09-29 12:29:35 -0300

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.215.98.8:9000/',
+  serverUrl: 'http://54.219.142.141:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'd29baa854a79a4a7e333997b397dd478dde194fe',
+	    'sonar.login': '6377a64cc006d3ad9c8fd599634c9676ab120ba8',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
